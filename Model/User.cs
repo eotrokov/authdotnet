56fed6b9f1e357918537d728805491740f5de809
eotrokov <eotrokov@logrocon.com>
2018-08-08 00:18:04 +0300

    init

---
+++ Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Model
+{
+    public class User
+    { 
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    
+    public enum UserRole
+    {
+        Normal = 0,
+        Admin = 1
+    }
+}
---
+++ WebApplication1/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repository;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Model;
+
+namespace WebApplication1
+{
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var u = new UserRepository().GetUser(user.Username);
+            if (u == null)
+                return NotFound("The user was not found.");
+            var credentials = u.Password.Equals(user.Password);
+            if (!credentials) 
+                return  Forbid("The username/password combination was wrong.");
+            return Ok(TokenManager.GenerateToken(user.Username));
+        }
+        
+        [HttpGet]
+        public IActionResult Validate(string token, string username)
+        {
+            bool exists = new UserRepository().GetUser(username) != null;
+            if (!exists) return NotFound("The user was not found.");
+            string tokenUsername = TokenManager.ValidateToken(token);
+            if (username.Equals(tokenUsername))
+                return Ok();
+            return BadRequest();
+        }
+    }
+}
